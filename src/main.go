f333bba5ed78a2bca74cb86bcc379a43b2d96cf6
James Sampica <jtsampica@gmail.com>
2024-01-02 20:26:39 -0600

    removing some garbage

--- src/main.go
+++ src/main.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"net/http"
-	"os"
 	"text/template"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", homeHandler)
-	http.ListenAndServe(":"+port, mux)
+
+	http.ListenAndServe(":80", mux)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
