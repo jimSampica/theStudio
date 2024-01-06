beb1bad9b9abcccac83f427554cb8a3a77251d7e
James Sampica <jtsampica@gmail.com>
2024-01-06 10:29:07 -0600

    adding a layout

--- src/main.go
+++ src/main.go
@@ -9,16 +9,21 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	homeTemplate = template.Must(template.ParseFiles("pages/layout.html"))
+	pagePaths := []string{
+		"pages/layout.go.html",
+		"pages/home.go.html",
+	}
+
+	templates = template.Must(template.ParseFiles(pagePaths...))
 	mux.HandleFunc("/", homeHandler)
 
 	http.ListenAndServe(":80", mux)
 }
 
-var homeTemplate *template.Template
+var templates *template.Template
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	homeTemplate.Execute(w, page{PageTitle: "Hello World"})
+	templates.ExecuteTemplate(w, "layout", nil)
 }
 
 type page struct {
