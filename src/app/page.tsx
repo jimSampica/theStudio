44193eda85e4b4cc6ec8e933ff8146b24588766a
James Sampica <jtsampica@gmail.com>
2024-05-13 22:04:39 -0500

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
+        <h1>Hello world</h1>
         <div className={styles.wave}></div>
     </main>
   );
