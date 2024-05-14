5b78b5f6ebf43902c7622cce3aa6856548514ed6
James Sampica <jtsampica@gmail.com>
2024-05-13 22:32:01 -0500

    wip

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,9 @@
+import styles from "./navbar.module.css";
+
+export default function Navbar() {
+    return (
+      <div className={styles.navbar}>
+          The Studio
+      </div>
+    );
+  }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Comfortaa } from "next/font/google";
 import "./globals.css";
-import Navbar from './navbar'
+import Navbar from './components/navbar'
 
 const comfortaa = Comfortaa({ subsets: ["latin"] });
 
