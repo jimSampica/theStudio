cb02399163edb8dd01f181cf4fbf00ac7ac0f631
James Sampica <jtsampica@gmail.com>
2024-05-20 16:35:06 -0500

    nav

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,30 @@
 
 export default function Navbar() {
-    return (
-      <nav className="text-danger">
-          <div>The Studio</div>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">TheStudio</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navControls">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navControls">
+          <ul className="navbar-nav me-auto mb-2">
+            <li className="nav-item">
+              <a className="nav-link" href="#">2-D Art</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">3-D Art</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Graphic Design</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Ceramics</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
