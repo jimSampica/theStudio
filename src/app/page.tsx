ee759ed89c0e9cdb5aafcfedb68361614b206c6c
James Sampica <jtsampica@gmail.com>
2024-05-25 06:51:52 -0500

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,13 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-        <h2>Featured Student Work</h2>
-        
-        <div className={styles.topwave}>
-        </div>
-        <div className={styles.bottomwave}>
-          <div className="d-flex align-items-center justify-content-center">
-            <p>testtestest</p>
-          </div>
-        </div>
+      <h2>Featured Student Work</h2>
+
+      <div className={styles.topwave}>
+      </div>
+      <div className={styles.bottomwave + " d-flex align-items-center justify-content-center"}>
+        <p>testtestest</p>
+      </div>
     </main>
   );
 }
