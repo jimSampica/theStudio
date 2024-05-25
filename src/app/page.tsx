b485e929daecfff6cc7015a350b61e5410326f34
James Sampica <jtsampica@gmail.com>
2024-05-25 07:27:21 -0500

    carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import styles from "./page.module.css";
+import Image from 'next/image'
+import slide1Pic from './media/1.jpg'
+import slide2Pic from './media/2.jpg'
+import slide3Pic from './media/3.jpg'
 
 export default function Home() {
   return (
@@ -8,7 +12,32 @@ export default function Home() {
       <div className={styles.topwave}>
       </div>
       <div className={styles.bottomwave + " d-flex align-items-center justify-content-center"}>
-        <p>testtestest</p>
+        <div id="carouselExampleIndicators" className="carousel slide">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <Image src={slide1Pic} height={300} alt="..." />
+            </div>
+            <div className="carousel-item">
+              <Image src={slide2Pic} height={300} alt="..." />
+            </div>
+            <div className="carousel-item">
+              <Image src={slide3Pic} height={300} alt="..." />
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
     </main>
   );
