5b108ba935b389a80fc5f4d3489ffdfd405b8109
James Sampica <jtsampica@gmail.com>
2024-05-25 07:32:02 -0500

    rename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,7 @@ export default function Home() {
       <div className={styles.topwave}>
       </div>
       <div className={styles.bottomwave + " d-flex align-items-center justify-content-center"}>
-        <div id="carouselExampleIndicators" className="carousel slide">
-          <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
+        <div id="homeCarousel" className="carousel slide">
           <div className="carousel-inner">
             <div className="carousel-item active">
               <Image src={slide1Pic} alt="..." />
@@ -29,11 +24,11 @@ export default function Home() {
               <Image src={slide3Pic} alt="..." />
             </div>
           </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <button className="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Previous</span>
           </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <button className="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Next</span>
           </button>
