8ad9df5d74fa7623dda455a81af5339029f4df26
James Sampica <jtsampica@gmail.com>
2024-06-01 15:59:57 -0500

    wip

--- src/app/components/wavey/wavey.tsx
+++ src/app/components/wavey/wavey.tsx
@@ -1,21 +1,23 @@
 import styles from "./wavey.module.css";
 
 interface WaveyProps {
-    children: React.ReactNode, 
+    children: React.ReactNode,
     offsetPercent?: `${number}%`,
     topMargin?: number;
-  }
+}
 
 export default function Wavey({
     children,
     offsetPercent = "25%",
     topMargin = 0
-  }: Readonly<WaveyProps>)  {
+}: Readonly<WaveyProps>) {
     return (
-        <div className={styles.wavey + " d-flex align-items-center justify-content-center"} 
-             style={{["--leftOffset" as string]: offsetPercent, 
-                     ["--topMargin" as string]: topMargin}}>
+        <div className={styles.wavey + " d-flex align-items-center justify-content-center"}
+            style={{
+                ["--leftOffset" as string]: offsetPercent,
+                ["--topMargin" as string]: topMargin
+            }}>
             {children}
         </div>
     );
-  }
+}
