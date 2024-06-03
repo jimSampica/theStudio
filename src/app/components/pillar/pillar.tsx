1d29e1a628107a05b3f38232833a9c03a070ed54
James Sampica <jtsampica@gmail.com>
2024-06-02 19:37:49 -0500

    del

--- src/app/components/pillar/pillar.tsx
+++ src/app/components/pillar/pillar.tsx
@@ -1,15 +1,11 @@
 import styles from "./pillar.module.css";
 
 interface PillarProps {
-    children: React.ReactNode,
-    offsetPercent?: `${number}%`,
-    topMargin?: number;
+    children: React.ReactNode
 }
 
 export default function Pillar({
     children,
-    offsetPercent = "25%",
-    topMargin = 0
 }: Readonly<PillarProps>) {
     return (
         <div className={styles.pillar}>
