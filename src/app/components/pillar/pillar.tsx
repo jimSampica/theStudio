72b98a7c80480c7b0f821262e06ad5630f381875
James Sampica <jtsampica@gmail.com>
2024-06-10 08:59:41 -0500

    pillar colors

--- src/app/components/pillar/pillar.tsx
+++ src/app/components/pillar/pillar.tsx
@@ -4,35 +4,17 @@ interface PillarProps {
     children: React.ReactNode
 }
 
-export function BluePillar({
-    children
-}: Readonly<PillarProps>) {
-    return (
-        <div className={styles.pillar} style={{ ["--pillar-color" as string]: "#ff84f1" }}>
-            {children}
-        </div>
-    );
-}
-
 export function PinkPillar({
     children
-}: Readonly<PillarProps>) {
-    return (
-        <div className={styles.pillar} style={{ ["--pillar-color" as string]: "#edff65" }}>
-            {children}
-        </div>
-    );
-}
+}: Readonly<PillarProps>) { return ColorPillar(children, "#ff84f1") } 
 
 export function YellowPillar({
     children
-}: Readonly<PillarProps>) {
-    return (
-        <div className={styles.pillar} style={{ ["--pillar-color" as string]: "#96e6ff" }}>
-            {children}
-        </div>
-    );
-}
+}: Readonly<PillarProps>) { return ColorPillar(children, "#edff65") } 
+
+export function BluePillar({
+    children
+}: Readonly<PillarProps>) { return ColorPillar(children, "#96e6ff") } 
 
 export function OrangePillar({
     children
@@ -42,7 +24,7 @@ type HexColorCode = `#${string}`;
 
 function ColorPillar(children: React.ReactNode, color: HexColorCode){
     return (
-        <div className={styles.pillar} style={{ ["--pillar-color" as string]: "#ffaf80" }}>
+        <div className={styles.pillar} style={{ ["--pillar-color" as string]: color }}>
             {children}
         </div>
     );
