4f0748dc9d3b32cdff49656c81ee471deaa7431f
James Sampica <jtsampica@gmail.com>
2024-06-28 19:56:13 -0500

    lesson 1

--- src/app/2d-art/page.tsx
+++ src/app/2d-art/page.tsx
@@ -3,9 +3,10 @@ import Link from "next/link"
 export default function Page() {
     return (
       <main className="container">
+        <h2 className="my-3">2D Art</h2>
         <ol>
           <li>
-            <Link href="./2d-art">Lesson One: Great Expectations</Link>
+            <Link href="./expectations">Lesson One: Great Expectations</Link>
           </li>
         </ol>
       </main>
--- src/app/expectations/page.tsx
+++ src/app/expectations/page.tsx
@@ -1,8 +1,11 @@
 export default function Page() {
     return (
       <main className="container">
-        <h2>Lesson One:</h2>
+        <h2 className="mt-3 mb-2">Lesson One:</h2>
         <h3>Great Expectations</h3>
+        <h4 className="mt-3">Instructions</h4>
+        <p>Please watch this video in its entirety and answer the questions that follow in your sketchbook.</p>
+
       </main>
     );
   }
