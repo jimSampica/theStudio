fbd8fc15a2b4805a88d0ca8b9e8a426e6e039900
James Sampica <jtsampica@gmail.com>
2024-07-02 00:14:51 -0500

    design tweaks

--- src/app/expectations/page.tsx
+++ src/app/expectations/page.tsx
@@ -4,14 +4,10 @@ import WelcomePic from "/public/static/images/welcome_to_art.png";
 export default function Page() {
   return (
     <main className="container">
-      <h2 className="mt-3 mb-2">Lesson One:</h2>
-      <h3>Great Expectations</h3>
-
       <div className="row">
-        <div className="col-md-6">
-          <Image src={WelcomePic} alt="Welcome to Art!" className="img-fluid" />
-        </div>
-        <div className="col-md-6">
+        <div className="col-md-7">
+        <h2 className="mt-3 mb-2">Lesson One:</h2>
+        <h3>Great Expectations</h3>
           <h4 className="mt-3">Instructions</h4>
           <p>Please watch <a href="https://app.screencastify.com/v3/watch/XlnDJ9alAvYqs1llN6t4">this video</a> in its entirety and answer the questions that follow in your sketchbook.</p>
 
@@ -28,6 +24,9 @@ export default function Page() {
             </li>
           </ul>
           <p>Let Mrs. Sam know when you complete this and she will discuss your next steps.</p>
+        </div>
+        <div className="col-md-5">
+          <Image src={WelcomePic} alt="Welcome to Art!" width={375} className="img-fluid" />
         </div>
       </div>
     </main>
