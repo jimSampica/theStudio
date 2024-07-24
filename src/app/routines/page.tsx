742a1a87a5a2ee627e7977433139cb16100f74fe
James Sampica <jtsampica@gmail.com>
2024-07-23 20:57:02 -0500

    content

--- src/app/routines/page.tsx
+++ src/app/routines/page.tsx
@@ -21,13 +21,21 @@ export default function Page() {
               &nbsp;and draw the assigned prompt in box a.
             </li>
             <li>
-              Please list the routine order.
+              Where can all class information be found when  you are gone, late, virtual learning or anytime?
             </li>
             <li>
-              After cleaning up, where should you sit? why?
+              What do you think the benefits or drawbacks of this self paced class might be?
             </li>
           </ul>
           <p>Let Mrs. Sam know when you complete this and she will discuss your next steps.</p>
+          <h4>Studio Time</h4>
+          <ol>
+            <li>Design on a half-sheet printer paper and as always keep it appropriate. 
+              This is not a private book, if you wouldn't want me or a parent to see your picture don't draw it.</li>
+            <li>Take your final design and draw it again on your sketchbook, you know, but nicer.</li>
+            <li>Your name should be <strong>highly visible</strong>.</li>
+            <li>Take care of this book, always put it back in the bin. You only get one sketchbook!</li>
+          </ol>
           <h4>Additional Resources</h4>
           <Image src={Howto} alt="how to learn with video lessons.png" width={300} className="img-fluid" />
         </div>
