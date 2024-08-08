16bed400015aa48033289a9ec800677a9c96617b
James Sampica <jtsampica@gmail.com>
2024-08-07 20:14:53 -0500

    open link

--- src/app/lesson-three/page.tsx
+++ src/app/lesson-three/page.tsx
@@ -1,16 +1,9 @@
-"use client";
-
 import Image from "next/image";
 import SplashPic from "/public/static/images/routines.jpg";
 import Howto from "/public/static/images/learning_video_lessons.png";
 import Link from "next/link";
 
 export default function Page() {
-
-    function imageClick() {
-        alert("test");
-    }
-
     return (
         <main className="container">
             <div className="row">
@@ -36,7 +29,9 @@ export default function Page() {
                     </ul>
                     <p>Let Mrs. Sam know when you complete this and she will discuss your next steps.</p>
                     <h4>Additional Resources</h4>
-                    <Image src={Howto} alt="how to learn with video lessons.png" width={300} className="img-fluid" onClick={imageClick} style={{ "cursor": "zoom-in" }} />
+                    <Link href={Howto.src} target="_blank">
+                        <Image src={Howto} alt="how to learn with video lessons.png" width={300} className="img-fluid" style={{ "cursor": "zoom-in" }} />
+                    </Link>
                 </div>
                 <div className="col-md-5">
                     <Image src={SplashPic} alt="Routines" width={375} className="img-fluid" />
