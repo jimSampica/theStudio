4f61268cb5354f91d5ffc7f88b8b62a8e2e7f9e5
James Sampica <jtsampica@gmail.com>
2024-09-04 20:45:17 -0500

    lesson eight 2d art

---
+++ src/app/2d-art/lesson-eight/page.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+
+export default function Page() {
+    return (
+        <main className="container">
+            <div className="row">
+                <div className="col-md-7">
+                    <h2 className="mt-3 mb-2">Lesson Eight: Creativity</h2>
+                    <h4 className="mt-3">Instructions</h4>
+                    <p>Please watch <a href="https://app.screencastify.com/v3/watch/pb7054tKiCOgKcHskYC1" target="_blank">Creativity</a>
+                        &nbsp;and&nbsp;<a href="https://app.screencastify.com/v3/watch/fXqmYfjKokH7eoshYD0D" target="_blank">Creativity isn&apos;t...</a>
+                        &nbsp;in its entirety and answer the questions that follow in your sketchbook.</p>
+                    <h4 className="mt-3">Sketchbook</h4>
+                    <ul style={{ ["listStyleType" as string]: "lower-alpha" }}>
+                        <li>
+                            Create a drawing out of three circles
+                        </li>
+                        <li>
+                            What three things help us be more creative (Listed on the slide)?
+                        </li>
+                        <li>
+                            Why should we avoid comparison?
+                        </li>
+                    </ul>
+                    <h4 className="mt-3">Studio time</h4>
+                    <p>Materials: watercolor and paper <br />Abstract emotions game</p>
+                </div>
+            </div>
+        </main>
+    );
+}
