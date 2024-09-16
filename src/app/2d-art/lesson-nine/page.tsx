18aa5c48722bc40682bf93609f9e7322beabadb6
James Sampica <jtsampica@gmail.com>
2024-09-16 18:36:12 -0500

    fix copyright links

--- src/app/2d-art/lesson-nine/page.tsx
+++ src/app/2d-art/lesson-nine/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
                 <div className="col-md-7">
                     <h2 className="mt-3 mb-2">Lesson Nine: Copyright</h2>
                     <h4 className="mt-3">Instructions</h4>
-                    <p>Please watch <a href="https://app.screencastify.com/v3/watch/pb7054tKiCOgKcHskYC1" target="_blank">Copyright Infringement</a>
+                    <p>Please watch <a href="https://app.screencastify.com/v3/watch/ZfBDnrfPotsUfTKgra5A" target="_blank">Copyright Infringement</a>
                         &nbsp;in its entirety and answer the questions that follow in your sketchbook.</p>
                     <h4 className="mt-3">Sketchbook</h4>
                     <ul style={{ ["listStyleType" as string]: "lower-alpha" }}>
