64aa4496c6d9eae0f49c2f25dd6f96125de37dd2
James Sampica <jtsampica@gmail.com>
2024-09-18 18:08:55 -0500

    fixes

--- src/app/2d-art/lesson-10/page.tsx
+++ src/app/2d-art/lesson-10/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
         <main className="container">
             <div className="row">
                 <div className="col-md-7">
-                    <h2 className="mt-3 mb-2">Lesson 10: Drawing Materials</h2>
+                    <h2 className="mt-3 mb-2">Lesson Ten: Drawing Materials</h2>
                     <h4 className="mt-3">Instructions</h4>
                     <p>Please watch <a href="https://app.screencastify.com/v3/watch/ahPuiMKr2h4tqiOgqB9B" target="_blank">Drawing with Charcoal</a>
                         &nbsp;in its entirety and answer the questions that follow in your sketchbook.</p>
--- src/app/2d-art/page.tsx
+++ src/app/2d-art/page.tsx
@@ -32,6 +32,9 @@ export default function Page() {
         <li>
           <Link href="/2d-art/lesson-9">Lesson Nine: Copyright</Link>
         </li>
+        <li>
+          <Link href="/2d-art/lesson-10">Lesson Ten: Drawing Materials</Link>
+        </li>
       </ol>
     </main>
   );
