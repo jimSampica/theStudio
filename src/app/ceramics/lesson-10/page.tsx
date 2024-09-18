3038ce5d4e5b8d07cbd61c1348791a08969bd683
James Sampica <jtsampica@gmail.com>
2024-09-18 18:13:55 -0500

    fixes

--- src/app/ceramics/lesson-10/page.tsx
+++ src/app/ceramics/lesson-10/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
                 <div className="col-md-7">
                     <h2 className="mt-3 mb-2">Lesson Ten: Wedging</h2>
                     <h4 className="mt-3">Instructions</h4>
-                    <p>Please watch <a href="https://app.screencastify.com/v2/watch/MlRkFixOYkEqePMIUvH" target="_blank">Beginning Graphic Design</a>
+                    <p>Please watch <a href="https://app.screencastify.com/v2/watch/MlRkFixOYkEqePMIUvH" target="_blank">???</a>
                         &nbsp;in its entirety and answer the questions that follow in your sketchbook.</p>
                     <h4 className="mt-3">Sketchbook</h4>
                     <ul style={{ ["listStyleType" as string]: "lower-alpha" }}>
