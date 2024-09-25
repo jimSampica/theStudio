530261758cb7f24c8091eca4838030a9bf9db7e7
James Sampica <jtsampica@gmail.com>
2024-09-24 21:37:12 -0500

    ceramics lesson 12 (WIP)

---
+++ src/app/ceramics/lesson-12/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link"
+
+export default function Page() {
+    return (
+        <main className="container">
+            <div className="row">
+                <div className="col-md-7">
+                    <h2 className="mt-3 mb-2">Lesson: Ceramic Tools</h2>
+                    <h4 className="mt-3">Instructions</h4>
+                    <p>Scroll over picture to get to know more about each ceramic tool:</p>
+                    <h4 className="mt-3">Sketchbook</h4>
+                    <ul style={{ ["listStyleType" as string]: "lower-alpha" }}>
+                        <li>
+                            Please use the&nbsp;
+                            <Link href="/prompt-generator">random prompt generator</Link>
+                        </li>
+                        <li>
+                            What do you use a wire tool for?
+                        </li>
+                        <li>
+                            What is the best tool for making slabs in our class?
+                        </li>
+                    </ul>
+                    <h4 className="mt-3">Studio time</h4>
+                    <p>Continue first hand-built project.</p>
+                </div>
+            </div>
+        </main>
+    );
+}
