fcc183d8b07533bd86dfc21bdcec6eb91272f679
James Sampica <jtsampica@gmail.com>
2024-09-27 15:22:10 -0500

    css + events

--- src/app/ceramics/lesson-12/ceramicTools/ceramicTools.tsx
+++ src/app/ceramics/lesson-12/ceramicTools/ceramicTools.tsx
@@ -5,8 +5,13 @@ import Image from "next/image";
 import ToolsImg from "/public/static/images/ceramic_tools.png";
 
 export function CeramicTools() {
+
+  function toolAreaClicked(){
+    alert("test");
+  }
+
   return (
-    <div className={styles["answer"]}>
+    <div onClick={toolAreaClicked} className={styles["tool-area"]}>
       <Image src={ToolsImg} alt="" />
     </div>
   );
