5ad1ab58e676f95b636c4d24d3f576bc9ccb0d8f
James Sampica <jtsampica@gmail.com>
2024-10-15 15:33:36 -0500

    add video

--- src/app/ceramics/lesson-16/page.tsx
+++ src/app/ceramics/lesson-16/page.tsx
@@ -7,7 +7,8 @@ export default function Page() {
                 <div className="col-md-7">
                     <h2 className="mt-3 mb-2">Lesson: Sculpture</h2>
                     <h4 className="mt-3">Instructions</h4>
-                    <p></p>
+                    <p>Please watch <a href="https://app.screencastify.com/v3/watch/hSJDQjE2IfQHWOg3tILg" target="_blank">Sculpture</a>
+                        &nbsp;in its entirety and answer the questions that follow in your sketchbook.</p>
                     <h4 className="mt-3">Sketchbook</h4>
                     <ul style={{ ["listStyleType" as string]: "lower-alpha" }}>
                         <li>
