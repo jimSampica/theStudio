561b0dd5c14fb53f3ee3965b751b2cacc9109ec7
James Sampica <jtsampica@gmail.com>
2024-10-23 19:07:47 -0500

    graphic design lesson 19 package design lid

---
+++ src/app/graphic-design/lesson-19/page.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <main className="container">
+            <div className="row">
+                <div className="col-md-7">
+                    <h2 className="mt-3 mb-2">Lesson: Package Design Lid</h2>
+                    <h4 className="mt-3">Instructions</h4>
+                    <p>Please watch <a href="https://app.screencastify.com/v3/watch/n5XzE2Ef5pnqqGFVyVsG" target="_blank">Package Design</a>
+                        &nbsp;in its entirety and answer the questions that follow in your sketchbook.</p>
+                    <h4 className="mt-3">Sketchbook</h4>
+                    <ul style={{ ["listStyleType" as string]: "lower-alpha" }}>
+                        <li>
+                            Please use the&nbsp;
+                            <Link href="/prompt-generator">random prompt generator</Link>
+                        </li>
+                        <li>
+                            How many instances of Logo should you have on your box?
+                        </li>
+                        <li>
+                            Why do the flaps on our lid design contain assets (pictures/text) going outward instead of right side up?
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    );
+}
