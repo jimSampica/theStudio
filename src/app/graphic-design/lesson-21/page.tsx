d66f0f141b8a7cfc3715ad7b8cd7853761e8e2b6
James Sampica <jtsampica@gmail.com>
2024-11-10 11:16:55 -0600

    graphic design lesson 21 clients

---
+++ src/app/graphic-design/lesson-21/page.tsx
@@ -0,0 +1,50 @@
+export default function Page() {
+    return (
+        <main className="container">
+            <div className="row">
+                <div className="col-md-7">
+                    <h2 className="mt-3 mb-2">Lesson: Clients</h2>
+                    <h4 className="mt-3">Instructions</h4>
+                    <p>
+                        Read through the following client sheets and look at the example half-fold brochure attached.
+                        Your job will be to read to your clients wants and envision a brochure that meets their needs.
+                    </p>
+                    <ol>
+                        <li>
+                            <a href="https://docs.google.com/document/d/139PGvpiXDuSC_tvyau0PCkxHqgSBS9owNk3kUN9002o/edit?usp=sharing" target="_blank">
+                                Chris&apos; client sheet
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://docs.google.com/document/d/1N6S7NGLIsl6kVN3KvGQu_wmGQUxywU2okYQZnvTULFE/edit?usp=sharing" target="_blank">
+                                Juan&apos;s client sheet
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://docs.google.com/document/d/1avGYyQVwlMirXYnVBHL8X2VWKg9e-sxzz5Rivc5kXvo/edit?usp=sharing" target="_blank">
+                                Cassidy&apos;s client sheet
+                            </a>
+                        </li>
+                    </ol>
+                    <p>
+                        <a href="https://docs.google.com/presentation/d/12W57tllP5fbef7H2DMyrOmj8IAsO2Ilhe4CTzX7jQvU/copy?usp=sharing" target="_blank">
+                            Brochure Template
+                        </a>
+                    </p>
+                    <h4 className="mt-3">Sketchbook</h4>
+                    <ul style={{ ["listStyleType" as string]: "lower-alpha" }}>
+                        <li>
+                            Create a mockup of a brochure here.
+                        </li>
+                        <li>
+                            What business are you going to choose from the client sheets?
+                        </li>
+                        <li>
+                            How can I use Contrast, Repetition, Alignment, and/or Proximity to create a brochure.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    );
+}
