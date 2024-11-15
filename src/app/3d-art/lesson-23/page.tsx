73a38495a000fc7b7384fb549db6e059ab612537
James Sampica <jtsampica@gmail.com>
2024-11-14 19:11:12 -0600

    fix heading Lesson: Found Objects

--- src/app/3d-art/lesson-23/page.tsx
+++ src/app/3d-art/lesson-23/page.tsx
@@ -5,9 +5,9 @@ export default function Page() {
         <main className="container">
             <div className="row">
                 <div className="col-md-7">
-                    <h2 className="mt-3 mb-2">Lesson: Community Art</h2>
+                    <h2 className="mt-3 mb-2">Lesson: Found Objects</h2>
                     <h4 className="mt-3">Instructions</h4>
-                    <p>Please watch <a href="https://app.screencastify.com/v3/watch/LMDytr9kNd6hrtnbmeIa" target="_blank">Community Art</a>
+                    <p>Please watch <a href="https://app.screencastify.com/v3/watch/LMDytr9kNd6hrtnbmeIa" target="_blank">Found Objects</a>
                         &nbsp;in its entirety and answer the questions that follow in your sketchbook.</p>
                     <h4 className="mt-3">Sketchbook</h4>
                     <ul style={{ ["listStyleType" as string]: "lower-alpha" }}>
