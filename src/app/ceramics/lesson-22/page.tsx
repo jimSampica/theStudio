5e6280dde821a16aa4f61323297885133df880f9
James Sampica <jtsampica@gmail.com>
2024-12-11 14:55:33 -0600

    add artist statement to 3d art and ceramics

--- src/app/ceramics/lesson-22/page.tsx
+++ src/app/ceramics/lesson-22/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Sketchbook2dPic from "/public/static/images/sketchbook_2d.png";
+import Link from "next/link";
+
 export default function Page() {
     return (
         <main className="container">
@@ -8,6 +12,11 @@ export default function Page() {
                     <p>Please watch <a href="https://app.screencastify.com/v3/watch/8OtOC2gCrJJJdlJ3e0Qf" target="_blank">Artist Statement</a>
                     &nbsp;in its entirety.</p>
                     <p>The completion of this lesson is designed to help you write your artist statement in conjunction with your last page in your sketchbook.</p>
+                </div>
+                <div className="col-12">
+                    <Link href={Sketchbook2dPic.src} target="_blank">
+                        <Image src={Sketchbook2dPic} alt="sketchbook 2d" className="img-fluid" style={{ "cursor": "zoom-in" }} />
+                    </Link>
                 </div>
             </div>
         </main>
