97a5060c3bd25c479574ba49f737398d4c180c39
James Sampica <jtsampica@gmail.com>
2025-02-02 13:15:57 -0600

    2d art routing updates

--- src/app/2d-art/page.tsx
+++ src/app/2d-art/page.tsx
@@ -25,13 +25,13 @@ export default function Page() {
           <Link href="/lesson-6">Lesson: Safety in the Classroom</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-7">Lesson: Cleaning</Link>
+          <Link href="/2d-art/cleaning">Lesson: Cleaning</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-8">Lesson: Creativity</Link>
+          <Link href="/2d-art/creativity">Lesson: Creativity</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-9">Lesson: Copyright</Link>
+          <Link href="/2d-art/copyright">Lesson: Copyright</Link>
         </li>
       </ol>
       <h3 className="h5">Skillbuilders</h3>
@@ -46,43 +46,43 @@ export default function Page() {
           <Link href="/2d-art/observational-drawing-2">Lesson: Observational drawing 2</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-13">Lesson: Color mixing 1</Link>
+          <Link href="/2d-art/color-mixing-1">Lesson: Color mixing 1</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-14">Lesson: Color mixing 2</Link>
+          <Link href="/2d-art/color-mixing-2">Lesson: Color mixing 2</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-15">Lesson: Painting Techniques 1</Link>
+          <Link href="/2d-art/painting-techniques-1">Lesson: Painting Techniques 1</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-16">Lesson: Painting Techniques 2</Link>
+          <Link href="/2d-art/painting-techniques-2">Lesson: Painting Techniques 2</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-17">Lesson: Weaving</Link>
+          <Link href="/2d-art/weaving">Lesson: Weaving</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-19">Lesson: Scratch Art</Link>
+          <Link href="/2d-art/scratch-art">Lesson: Scratch Art</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-20">Lesson: Printmaking Foam</Link>
+          <Link href="/2d-art/printmaking-foam">Lesson: Printmaking Foam</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-21">Lesson: Gelli Printing</Link>
+          <Link href="/2d-art/gelli-printing">Lesson: Gelli Printing</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-22">Lesson: Grid Drawing</Link>
+          <Link href="/2d-art/grid-drawing">Lesson: Grid Drawing</Link>
         </li>
       </ol>
       <h3 className="h5">Finishing</h3>
       <ol className="list-style-none">
         <li>
-          <Link href="/2d-art/lesson-18">Lesson: WOW Project</Link>
+          <Link href="/2d-art/wow-project">Lesson: WOW Project</Link>
         </li>
         <li>
-          <Link href="/2d-art/lesson-23">Lesson: Artist Statement</Link>
+          <Link href="/2d-art/artist-statement">Lesson: Artist Statement</Link>
         </li>
         <li> 
-          <Link href="/2d-art/lesson-24">Lesson: Art Portfolio Creation</Link>
+          <Link href="/2d-art/art-portfolio-creation">Lesson: Art Portfolio Creation</Link>
         </li>
       </ol>
     </main>
