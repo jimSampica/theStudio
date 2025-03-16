e58254ea375d6e7a6a8092f8ab460566e359863a
James Sampica <jtsampica@gmail.com>
2025-03-16 16:36:05 -0500

    drag/drop wip

--- src/app/innovators/simple-machines-2/page.tsx
+++ src/app/innovators/simple-machines-2/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import styles from "./page.module.css";
+import { useState } from "react";
 import Image from "next/image";
 import img from "/public/static/images/simple_machines.png";
-import Link from "next/link";
 
 export default function Page() {
+    let [selectedAnswers, setSelectedAnswers] = useState<Answer[]>([]);
 
     function allowDrop(ev: any) {
+        console.log(ev);
         ev.preventDefault();
+        return false
     }
 
     function drag(ev: any) {
@@ -33,28 +36,39 @@ export default function Page() {
                 <li>Tell me the difference between a wedge and an inclined plane. How are they similar?</li>
                 <li>What simple machine uses a fulcrum and what is a fulcrum?</li>
             </ul>
-            <div className={styles["drop-targets"] + " row d-none"}>
+            <div className={styles["drop-targets"] + " d-flex flex-column d-none"}>
                 <div onDrop={drop} onDragOver={allowDrop} className={styles["drop-target"] + " col-md-2"}>
-                    <h5>Wedge</h5>
+                    <h5 className="m-3">Wedge</h5>
                 </div>
                 <div className={styles["drop-target"] + " col-md-2"}>
-                    <h5>Pulley</h5>
+                    <h5 className="m-3">Pulley</h5>
                 </div>
                 <div className={styles["drop-target"] + " col-md-2"}>
-                    <h5>Lever</h5>
+                    <h5 className="m-3">Lever</h5>
                 </div>
                 <div className={styles["drop-target"] + " col-md-2"}>
-                    <h5>Inclined Plane</h5>
+                    <h5 className="m-3">Inclined Plane</h5>
                 </div>
                 <div className={styles["drop-target"] + " col-md-2"}>
-                    <h5>Wheel and Axel</h5>
+                    <h5 className="m-3">Wheel and Axel</h5>
                 </div>
                 <div className={styles["drop-target"] + " col-md-2"}>
-                    <h5>Screw</h5>
+                    <h5 className="m-3">Screw</h5>
                 </div>
             </div>
 
-            <img id="drag1" className="d-none" src={img.src} draggable="true" onDragStart={drag}></img>
+            <img id="drag1" src={img.src} className="d-none" draggable="true" onDragStart={drag} style={{"width": "100px"}}></img>
         </main>
     );
 }
+
+const AllAnswers = [
+    "wedge",
+    "pulley",
+    "lever",
+    "plane",
+    "wheel",
+    "screw"
+] as const;
+
+type Answer = typeof AllAnswers[number];
