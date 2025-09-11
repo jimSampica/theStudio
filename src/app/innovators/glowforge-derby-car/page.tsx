f74b27d5452dcefcf63a00aa9bdc46dba2af36e1
James Sampica <jtsampica@gmail.com>
2025-09-10 20:32:02 -0500

    Glowforge Derby Car content updates

--- src/app/innovators/glowforge-derby-car/page.tsx
+++ src/app/innovators/glowforge-derby-car/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
         <main className="container">
             <h2 className="mt-3 mb-2">Lesson: Glowforge Derby Car</h2>
             <h4 className="mt-3">Instructions</h4>
-            <p>Please watch <a href="https://app.screencastify.com/v3/watch/Xixbjn5GitJqn3kVriFi" target="_blank">Glowforge Derby Car</a>
+            <p>Please watch <a href="https://app.screencastify.com/watch/evsKCB2Rj3WwOFljJcRJ" target="_blank">Glowforge Derby Car</a>
                 &nbsp;in its entirety and answer the questions that follow in your sketchbook.</p>
             <p>Let Mrs. Sam know when you complete this and she will discuss your next steps.</p>
             <h4>Workbook</h4>
@@ -15,7 +15,7 @@ export default function Page() {
                 <li>What should you name your Derby car file?</li>
                 <li>In a complete sentence tell me what surface numbers from the Derby Car drawings file are designable.</li>
             </ol>
-            <a href="https://docs.google.com/drawings/d/1Tru9e3HqEQVdudxLeYqD9YNHQH3M_oALOKN3JBeu-Bw/copy" target="_blank">Derby Car Template</a>
+            <a href="https://docs.google.com/drawings/d/1PUlMYligggCtPloV9-djwZQhVRwZ1SnZBeAwH8bphNI/copy" target="_blank">Derby Car Template</a>
             <br />
             <Image src={DerbyCarPic} alt="" width={500} className="img-fluid rounded mt-3" style={{ "cursor": "zoom-in" }} />
         </main>
